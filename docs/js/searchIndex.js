b3154354afb7f61681981f455e87b4e2a3679d13
Adolfo <adolfoinsti@gmail.com>
2024-10-29 10:10:32 +0100

    Primer commit

---
+++ docs/js/searchIndex.js
@@ -0,0 +1,14 @@
+Search.appendIndex(
+    [
+                {
+            "fqsen": "\\suma\u0028\u0029",
+            "name": "suma",
+            "summary": "The\u0020function\u0020\u0022suma\u0022\u0020takes\u0020two\u0020parameters\u0020and\u0020returns\u0020their\u0020sum.",
+            "url": "namespaces/default.html#function_suma"
+        },                {
+            "fqsen": "\\",
+            "name": "\\",
+            "summary": "",
+            "url": "namespaces/default.html"
+        }            ]
+);
