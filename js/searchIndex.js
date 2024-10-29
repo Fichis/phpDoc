a94832f998aac7b0e131b2828df26db84b8c1fa7
Fichis <70455772+Fichis@users.noreply.github.com>
2024-10-29 18:09:09 +0000

    Deploying to gh-pages from @ Fichis/phpDoc@656a1026ac4629cf2574f27a290907f83c481db6 🚀

--- js/searchIndex.js
+++ js/searchIndex.js
@@ -3,7 +3,7 @@ Search.appendIndex(
                 {
             "fqsen": "\\suma\u0028\u0029",
             "name": "suma",
-            "summary": "The\u0020function\u0020\u0022suma\u0022\u0020takes\u0020two\u0020parameters\u0020and\u0020returns\u0020their\u0020sum.",
+            "summary": "The\u0020function\u0020\u0022suma\u0022\u0020takes\u0020two\u0020parameters\u0020and\u0020returns\u0020their\u0020sum.\u0020Cambio",
             "url": "namespaces/default.html#function_suma"
         },                {
             "fqsen": "\\",
